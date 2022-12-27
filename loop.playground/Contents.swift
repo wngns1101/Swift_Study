45fe6fcacc41a013221154827bcf88766aa81dc4
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 <wngns0812@gmail.com>
2022-12-27 09:45:27 +0900

    loop

---
+++ loop.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+for i in 1...4{
+    print(i)
+}
+
+let array = [1, 2, 3, 4, 5]
+for i in array{
+    print(i)
+}
+
+var number = 5
+while number < 10{
+    number += 1
+}
+print(number)
+
+// 한 번은 반드시 실행
+var x = 6
+repeat{
+    x+=2
+} while x<5
+print(x)

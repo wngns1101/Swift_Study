4e32d4c99140f0504fe4c5449d7c63e6ee4c3009
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 <wngns0812@gmail.com>
2023-01-02 16:18:51 +0900

    typeCasting

--- typeCasting.playground/Contents.swift
+++ typeCasting.playground/Contents.swift
@@ -1,3 +1,53 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class MediaItem{
+    var name: String
+    init(name: String){
+        self.name = name
+    }
+}
+
+class Movie: MediaItem{
+    var director: String
+    init(name: String, director: String){
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+class Song: MediaItem{
+    var artist: String
+    init(name: String, artist: String){
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let library = [
+    Movie(name: "기생충", director: "봉준호"),
+    Song(name: "Buttor", artist: "BTS"),
+    Movie(name: "올드보이", director: "박찬욱"),
+    Song(name: "wonderwall", artist: "Oasis"),
+    Song(name: "Rain", artist: "이적")
+]
+
+var movieCount = 0
+var songCount = 0
+
+for item in library{
+    if item is Movie{
+        movieCount += 1
+    }else if item is Song{
+        songCount += 1
+    }
+}
+
+print("Media library contains \(movieCount) movie and \(songCount) songs")
+
+for item in library {
+    if let movie = item as? Movie {
+        print("Movie: \(movie.name), dir, \(movie.director)")
+    }else if let song = item as? Song{
+        print("Song: \(song.name), by, \(song.artist)")
+    }
+}

8a7d68f821c988676d021c66835d8eb6d046aac2
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 <wngns0812@gmail.com>
2023-01-03 18:57:16 +0900

    extension

---
+++ extension.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+extension Int {
+    var isEven: Bool{
+        return self % 2 == 0
+    }
+    var isOdd: Bool {
+        return self % 2 == 1
+    }
+}
+
+
+var number = 3
+
+number.isOdd
+number.isEven
+
+extension String {
+    func convertToInt() -> Int?{
+        return Int(self)
+    }
+}
+
+var string = "0"
+string.convertToInt()

38bb9f214ed7d7b778e2c40a690a7098f6439306
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 <wngns0812@gmail.com>
2023-01-06 15:49:07 +0900

    higherOrderFunction

--- higherOrderFunction .playground/Contents.swift
+++ higherOrderFunction .playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// map
+let numbers = [0, 1, 2, 3]
+let mapArray = numbers.map { (number) -> Int in
+    return number * 2
+}
+print("\(mapArray)")
+
+// filter
+let intArray = [10, 5, 20, 13, 4]
+let filterArray = intArray.filter{$0 > 5}
+    print("filter \(filterArray)")
+
+// reduce
+let someArray = [1,2,3,4,5]
+let reduceResult = someArray.reduce(0){
+    (result: Int, element: Int) -> Int in
+    print("\(result) + \(element)")
+    return result + element
+}
+
+print("reduce \(reduceResult)")
